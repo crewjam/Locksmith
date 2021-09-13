f3b5a14cd423a1ff5e833d14d983d5f2dbcd85e2
Ross Kinder <ross@nametag.co>
2021-09-13 08:41:54 -0400

    Add products to Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,12 @@
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+  products: [
+        .library(name: "Locksmith", targets: ["Locksmith"])
+  ],
+    targets: [
+        .target(name: "Locksmith", path: "Source")
+    ]
+                    
 )
